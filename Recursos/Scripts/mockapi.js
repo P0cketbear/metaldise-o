261e3c5344b4f0b496f8147f33074b313a24db5b
P0cketbear <luis.penaranda.ramos@ciisa.cl>
2022-07-11 05:10:09 -0400

    Solemne3

--- Recursos/Scripts/mockapi.js
+++ Recursos/Scripts/mockapi.js
@@ -1,30 +1,48 @@
-$(document).ready(function () {
-    obtener_datos()
-});
 
-function obtener_datos() {
-    $.ajax('https://62aa6b2b371180affbd50961.mockapi.io/ventas', {
-        type: 'GET',
-        success: function (data) {
-            for (var index = 0; index < data.length; index++) {
-                var id = data[index].id
-                var usuario = data[index].usuario
-                var producto = data[index].producto
-                var cantidad = data[index].cantidad
-                var monto = data[index].precio
-                var moneda = data[index].moneda
-                var fecha = data[index].fecha
-                var fila_tabla = '<tr>'
-                                +'<td>'+id+'</td>'
-                                +'<td>'+usuario+'</td>'
-                                +'<td>'+producto+'</td>'
-                                +'<td>'+cantidad+'</td>'
-                                +'<td>'+monto+' '+moneda+'</td>'
-                                +'<td>'+fecha+'</td>'
-                                '</tr>'
-            console.log(fila_tabla)
-            $("#tbody_ventas").append(fila_tabla);
+    fetch('https://mindicador.cl/api').then(function(response) {
+        return response.json();
+    }).then(function(dailyIndicators) {
+        document.getElementById("t_indicadores").innerHTML = 'INDICADORES FINANCIEROS  ' + dailyIndicators.uf.fecha.split("T")[0] 
+        document.getElementById("UF").innerHTML = '$' + dailyIndicators.uf.valor; 
+        document.getElementById("UTM").innerHTML = '$' + dailyIndicators.utm.valor;
+        document.getElementById("DOLAR").innerHTML = '$' + dailyIndicators.dolar.valor;
+        document.getElementById("EURO").innerHTML = '$' + dailyIndicators.euro.valor;
+        document.getElementById("IPC").innerHTML = dailyIndicators.ipc.valor+ "%"; 
+        document.getElementById("DESEMPLEO").innerHTML = dailyIndicators.tasa_desempleo.valor + "%";
+        document.getElementById("BTC").innerHTML = 'USD$' + dailyIndicators.bitcoin.valor;
+        document.getElementById("COBRE").innerHTML = 'USD$' + dailyIndicators.libra_cobre.valor;
+    }).catch(function(error) {
+        console.log('Requestfailed', error);
+    });   
+  
+    function Indicadores() {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+              confirmButton: 'btn btn-success',
+              cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+          })
+          
+          swalWithBootstrapButtons.fire({
+            title: 'Desea ver los Indicadores Financieros?',
+            text: "En caso de no querer realizar la accion presionar cancelar!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Si, redirigeme!',
+            cancelButtonText: 'No, Cancelar!',
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+                var url = "../metaldise-o/indicadores.html";
+                $(location).attr('href',url);
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire(
+                'Redirección cancelada.',
+              )
             }
-        }
-    });
-}
+          })
+    }
